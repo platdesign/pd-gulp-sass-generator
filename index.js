4e30076619027bcfa78bfb519219af28440656e5
Christian Blaschke <mail@platdesign.de>
2015-01-24 14:27:51 +0100

    init

---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+var pdGulpBaseTask = require('pd-gulp-base-task');
+//var pdGulpBaseTask = require('../pd-gulp-base-task');
+
+
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var livereload = require('gulp-livereload');
+var autoprefixer = require('gulp-autoprefixer');
+var fs = require('fs');
+var watch = require('gulp-watch');
+
+
+module.exports = function pdGulpTaskJS(options) {
+	var task = pdGulpBaseTask(options);
+
+	task.worker(function(config, callback) {
+
+		function compile() {
+
+			var stream = gulp.src(config.src)
+				.pipe( plumber({
+					errorHandler: function(err) {
+						console.log(err.message);
+						this.emit('end');
+					}
+				}) )
+				.pipe( sass( config.sass || {} ) )
+				.pipe( autoprefixer(config.autoprefixer || 'last 3 versions', '> 1%', 'ie 8') );
+
+				if(config.use) {
+					stream = task.helper.useOnStream(config.use, stream);
+				}
+
+				if(task.options.banner) {
+					stream = stream.pipe( task.helper.banner(task.options.banner) );
+				}
+
+				if(config.livereload) {
+					stream = stream.pipe( livereload(config.livereload) );
+				}
+
+			return stream.pipe( gulp.dest( config.dest ) );
+		}
+
+
+		if(config.watch) {
+			watch(config.watch===true ? config.src : config.watch, function() {
+				compile();
+			});
+		}
+
+		compile().on('end', callback);
+
+	});
+
+	return task.gulpHandler();
+};
